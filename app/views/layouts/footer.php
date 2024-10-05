bc5d3f79eff5a587162eb6ac9653b00e6b9c741c
Nghia2515306 <ntn8530@gmail.com>
2024-10-05 20:23:38 +0700

    Nghia commit 2

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>footer</title>
-    <link rel="stylesheet" href="http://localhost/eproject/app/assets/css/footer.css">
+    
 <!-- goi no deo an -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flat-icons/css/flat-icons.min.css"> -->
 </head>
