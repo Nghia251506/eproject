d4047c944022c3847dca3d749b66787a2d62a30b
VIKINGS BACH DANG <ntn8530@gmail.com>
2024-10-09 21:56:16 +0700

    done midleware and login, register

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://kit.fontawesome.com/453b49545e.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="http://localhost/eproject/app/assets/css/header.css">
   <title>Header Example</title>
 </head>
 <body>
